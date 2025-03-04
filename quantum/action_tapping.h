246b6b5ba8d9864a12ea9f2ff3f020f6585b7800
Sebastian Haigh <sebhaigh27@googlemail.com>
2025-03-04 18:55:37 +0000

    Add corne keyboard layout

--- quantum/action_tapping.h
+++ quantum/action_tapping.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* period of tapping(ms) */
 #ifndef TAPPING_TERM
-#    define TAPPING_TERM 200
+#    define TAPPING_TERM 1000
 #endif
 
 /* period of quick tap(ms) */
